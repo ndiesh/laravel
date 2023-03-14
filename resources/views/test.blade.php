5f0a9170a970274565ccde1010205f2d26e9d211
SH <ndiesh@emato.net>
2023-03-14 14:21:47 +0900

    oop

---
+++ resources/views/test.blade.php
@@ -0,0 +1,1 @@
+<h1>Function</h1>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test_apple_master', function () {
+Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/oop', function () {
+    return view('test');
 });
 
 //master_11
