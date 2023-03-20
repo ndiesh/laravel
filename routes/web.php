90a2c7f7fe45d1cfa65c79ec7bd7f497571f26e4
SH <ndiesh@emato.net>
2023-03-20 13:23:28 +0900

    voyager

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
